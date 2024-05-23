dd88ba109d32a3de5c1ba62eb6f026969c85d0c4
Lewsha24 <sadkovoi0@gmail.com>
2024-05-23 21:24:57 +0700

    Start Lerning

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from "react";
 
 function App() {
-  return (
+    const formRef = useRef(null)
+    const inputNameRef = useRef(null)
+    const [value, setValue] = useState({
+      // name: '',
+      // lastname: '',
+      // middlename: '',
+  });
+    const handleBegin = () => {
+        inputNameRef.current.focus()
+    }
+
+  const handleChange = (event) => {
+        if(event.target.value.includes('q')) {
+            inputNameRef.current.blur()
+        }else {
+            setValue((prevState) => ({
+              ...prevState,
+              [event.target.name]: event.target.value
+            }))
+        }
+  }
+
+  const handleSubmit = (event) => {
+      //Останови выполнение по мнению браузера
+      event.preventDefault()
+      console.log(value)
+      formRef.current.reset();
+  }
+
+
+  const handleReset = () => {
+      setValue({})
+  }
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+          <form style={
+              {display: 'grid',}
+          }
+                ref={formRef}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                onReset={handleReset}
+          >
+               <button onClick={handleBegin}>Начать заполнять форму</button>
+              <input
+                  ref={inputNameRef}
+                  value={value.name}
+                  type="text"
+                  name="name"
+                  placeholder="Имя"
+                  onFocus={() => console.log('onFocus')}
+                  onBlur={() => console.log('Blur')}
+              />
+              <input value={value.lastname} type="text" name="lastname" placeholder="Фамилия"/>
+              <input value={value.middlename} type="text" name="middlename" placeholder="Отчество"/>
+              <button type={"submit"}>Submit</button>
+              <button type="reset">Reset</button>
+          </form>
+      <p>
+          {/*{value}*/}
+      </p>
       </header>
     </div>
   );
